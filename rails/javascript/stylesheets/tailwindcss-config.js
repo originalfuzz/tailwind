94406116e2b84892ab38a053a73a409448bde7ce
Lee McAlilly <leemcalilly@gmail.com>
2019-11-18 11:29:06 -0600

    Set up basic structure for tailwind css via webpacker

---
+++ rails/javascript/stylesheets/tailwindcss-config.js
@@ -0,0 +1,26 @@
+// Example `tailwind.config.js` file
+
+module.exports = {
+  important: true,
+  theme: {
+
+    container: {
+      center: true,
+    },
+
+    fontFamily: {
+      mono: [
+          '“Courier New”',
+          'Courier',
+          '“Lucida Sans Typewriter”',
+          '“Lucida Typewriter”',
+          'monospace',
+        ]
+    },
+
+    extend: {
+    }
+  },
+  variants: {
+  }
+}
